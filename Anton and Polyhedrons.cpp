c97efb66f5170b09041de81616165c71a11f7192
nilesh7757 <nileshmori7757@gmail.com>
2025-01-01 22:44:24 +0530

    Create Anton and Polyhedrons.cpp

---
+++ Anton and Polyhedrons.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+    int sum = 0;
+    for(int i = 0; i<n; i++){
+        cin>>s;
+        if(s == "Tetrahedron")  sum += 4;
+        else if(s == "Cube") sum += 6;
+        else if(s == "Octahedron") sum += 8;
+        else if(s == "Dodecahedron") sum += 12;
+        else if(s == "Icosahedron") sum += 20;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
